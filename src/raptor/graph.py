b545d51aa1bbd88c6c6786d27abdc783579f265b
kvpratama <kvpratama@gmail.com>
2025-06-19 11:29:45 +0900

    Update graph.py

--- src/raptor/graph.py
+++ src/raptor/graph.py
@@ -1,12 +1,14 @@
 from langgraph.graph import START, END, StateGraph
 from raptor.state import GraphState, GraphStateInput, GraphStateOutput
-from raptor.nodes import rag_node
+from raptor.nodes import init_retriever_node, rag_node
 from langgraph.checkpoint.memory import MemorySaver
 
 builder = StateGraph(GraphState, input=GraphStateInput, output=GraphStateOutput)
+builder.add_node("init_retriever_node", init_retriever_node)
 builder.add_node("rag_node", rag_node)
 
-builder.add_edge(START, "rag_node")
+builder.add_edge(START, "init_retriever_node")
+builder.add_edge("init_retriever_node", "rag_node")
 builder.add_edge("rag_node", END)
 
 # Compile
