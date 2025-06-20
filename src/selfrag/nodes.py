e5cb40523b90ba49511f17d5d45e7a1e7aa42647
kvpratama <kvpratama@gmail.com>
2025-06-20 17:51:17 +0900

    Update nodes.py

--- src/selfrag/nodes.py
+++ src/selfrag/nodes.py
@@ -1,4 +1,3 @@
-from langchain.schema import Document
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
